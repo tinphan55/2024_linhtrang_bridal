75ba82c5fba2bdee3fc4768b48fcd56967deb4e3
tinphan <tinph.1992@gmail.com>
2023-08-24 13:06:14 +0700

    add clients logic

--- clients/models.py
+++ clients/models.py
@@ -2,6 +2,26 @@ from django.db import models
 from django.db.models import  Sum, F
 
 
+class Province(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class District(models.Model):
+    name = models.CharField(max_length=100)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Ward(models.Model):
+    name = models.CharField(max_length=100)
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 class Client (models.Model):
     full_name = models.CharField(max_length= 50, verbose_name='Tên đầy đủ')
@@ -10,8 +30,12 @@ class Client (models.Model):
     created_date = models.DateTimeField(auto_now_add=True, verbose_name='Ngày tạo')
     last_order_date = models.DateTimeField(auto_now=True)
     address = models.CharField(max_length=100, null= True, blank = True, verbose_name='Địa chỉ')
+    ward = models.ForeignKey(Ward, on_delete=models.CASCADE,null= True, blank = True, verbose_name='Phường/Xã')
     birthday = models.DateField(null=True, blank = True, verbose_name='Ngày sinh')
-    #total_values = models.IntegerField (default=0)
+    note =  models.CharField(max_length= 200,null=True, blank = True, verbose_name='Ghi chú')
+    total_values = models.IntegerField (default=0)
+    str_total_values = models.CharField(max_length= 100,null=True, blank = True, verbose_name='Tổng tiền mua')
+
 
     class Meta:
         verbose_name = 'Quản lí Khách hàng'
@@ -20,4 +44,7 @@ class Client (models.Model):
     def __str__(self):
         return str(self.full_name) + '_' + str(self.code)
     
-    
+    def save(self, *args, **kwargs):
+        self.address = str(self.ward)+ str(', ')+ str(self.ward.district) + str(', ')+ str(self.ward.district.province)
+        super(Client , self).save(*args, **kwargs)
+
